a82fa16f38781dce0ba01eee8bfe8d81a4f1f3e6
Jakub Velička <jakub@velicka.eu>
2023-10-21 18:35:27 +0200

    Fix ip resolveru

--- app/Resolvers/IpAddressResolver.php
+++ app/Resolvers/IpAddressResolver.php
@@ -3,12 +3,13 @@ namespace App\Resolvers;
 
 use Illuminate\Support\Facades\Request;
 
-class IpAddressResolver implements \OwenIt\Auditing\Contracts\IpAddressResolver
+class IpAddressResolver implements \OwenIt\Auditing\Contracts\Resolver
 {
     /**
      * {@inheritdoc}
+     * @param Auditable $auditable
      */
-    public static function resolve(): string
+    public static function resolve(Auditable|\OwenIt\Auditing\Contracts\Auditable $auditable): string
     {
         return Request::header('HTTP_X_FORWARDED_FOR', '0.0.0.0');
     }
